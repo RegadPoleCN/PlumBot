a567a61e779a283518f4e0fdef4915b8fcf58c4e
RegadPoleCN <bilibilipopojun@hotmail.com>
2026-01-20 00:41:33 +0800

    fix: fix codec

--- plugin/src/main/kotlin/me/regadpole/plumbot/utils/ComponentUtils.kt
+++ plugin/src/main/kotlin/me/regadpole/plumbot/utils/ComponentUtils.kt
@@ -3,5 +3,5 @@ package me.regadpole.plumbot.utils
 import com.hypixel.hytale.server.core.Message
 
 fun getMessageFromString(msg: String): Message {
-    return Message.parse(msg)
+    return Message.raw(msg)
 }
